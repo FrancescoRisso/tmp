7c534ee5f4857dcf334c01be47811d78dde3f984
Francesco Risso <francesco@frisso.net>
2022-09-25 22:31:07 +0200

    .

--- docker/docker_run_cron.sh
+++ docker/docker_run_cron.sh
@@ -8,7 +8,8 @@ if [ "$cronSettings" = "" ] ;
 	then python3 PolitoMaterialDownload.py ;
 	else
 		rm -f /etc/cron.d/crontab;
-		echo "${cronSettings} python3 /${workdir}/PolitoMaterialDownload.py \"/${workdir}\" > /proc/1/fd/1 2>&1\n" >> /etc/cron.d/crontab;
+		echo "${cronSettings} python3 /${workdir}/PolitoMaterialDownload.py \"/${workdir}\" > /proc/1/fd/1 2>&1" >> /etc/cron.d/crontab;
+		echo " " >> /etc/cron.d/crontab;
 		chmod 0644 /etc/cron.d/crontab
 		/usr/bin/crontab /etc/cron.d/crontab;
 		ls -la /pmdUtils | echo;
